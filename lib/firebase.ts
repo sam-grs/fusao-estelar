c740bc63c011bcff46ccd3784a4b46864270eca9
Samira <grossisamira@hotmail.com>
2024-09-17 17:29:51 -0300

    feat: adds firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,25 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBwOdKVOMrYO0jWEQM6e7SQvm2Pnr-t9k",
-  authDomain: "coin-b6d8b.firebaseapp.com",
-  projectId: "coin-b6d8b",
-  storageBucket: "coin-b6d8b.appspot.com",
-  messagingSenderId: "491019852038",
-  appId: "1:491019852038:web:9c2ac608ce9514be97a687",
-};
+    apiKey: 'AIzaSyAPUOOh-jMf7L189SsUj3sajYP2VCDinkc',
+    authDomain: 'stellar-fusion-191e9.firebaseapp.com',
+    projectId: 'stellar-fusion-191e9',
+    storageBucket: 'stellar-fusion-191e9.appspot.com',
+    messagingSenderId: '503074817201',
+    appId: '1:503074817201:web:17d45b83f3aad72a64ce90',
+    measurementId: 'G-4P6RV8BY1B',
+}
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+auth.useDeviceLanguage()
+const analytics = getAnalytics(app)
