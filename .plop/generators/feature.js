8589c6573ea38b64f134967c37fa460a055b80f7
Samira <grossisamira@hotmail.com>
2024-09-25 15:37:34 -0300

    fix: changed paths in plopjs

--- .plop/generators/feature.js
+++ .plop/generators/feature.js
@@ -1,39 +1,39 @@
 module.exports = function (plop) {
-  plop.setGenerator("features", {
-    description: "React Feature Generator",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "Feature name",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "../app/features/{{ pascalCase name }}/{{ pascalCase name }}.tsx",
-        templateFile: "templates/features/feature.hbs",
-      },
-      {
-        type: "add",
-        path: "../app/features/{{ pascalCase name }}/{{ pascalCase name }}.styles.ts",
-        templateFile: "templates/features/styles.hbs",
-      },
-      {
-        type: "add",
-        path: "../app/features/{{ pascalCase name }}/__stories__/{{ pascalCase name }}.stories.tsx",
-        templateFile: "templates/features/feature-stories.hbs",
-      },
-      {
-        type: "add",
-        path: "../app/features/{{ pascalCase name }}/index.ts",
-        templateFile: "templates/features/index.hbs",
-      },
-      {
-        type: "add",
-        path: "../pages/{{ kebabCase name }}.tsx",
-        templateFile: "templates/features/pages.hbs",
-      },
-    ],
-  });
-};
+    plop.setGenerator('features', {
+        description: 'React Feature Generator',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Feature name',
+            },
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: '../src/features/{{ camelCase name }}/{{ pascalCase name }}.tsx',
+                templateFile: 'templates/features/feature.hbs',
+            },
+            {
+                type: 'add',
+                path: '../src/features/{{ camelCase name }}/{{ pascalCase name }}.styles.ts',
+                templateFile: 'templates/features/styles.hbs',
+            },
+            {
+                type: 'add',
+                path: '../src/features/{{ camelCase name }}/__stories__/{{ pascalCase name }}.stories.tsx',
+                templateFile: 'templates/features/feature-stories.hbs',
+            },
+            {
+                type: 'add',
+                path: '../src/features/{{ camelCase name }}/index.ts',
+                templateFile: 'templates/features/index.hbs',
+            },
+            {
+                type: 'add',
+                path: '../app/{{ camelCase name }}/{{ kebabCase name }}.tsx',
+                templateFile: 'templates/features/pages.hbs',
+            },
+        ],
+    })
+}
