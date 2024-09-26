848e41a59fe78abf377945446851d971f95edf3c
Samira <grossisamira@hotmail.com>
2024-09-25 22:15:17 -0300

    feat: adds routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,12 @@
+export const Routes = {
+    linkedin: 'https://www.linkedin.com/in/samira-grossi/',
+    home: '/',
+
+    register: '/auth/registro',
+    login: '/auth/login',
+    recoverPassword: '/auth/recuperar-senha',
+
+    listagem: '/listagem',
+    perfil: '/perfil',
+    galeria: '/galeria',
+}
