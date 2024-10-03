c4b99bb7d4151e86c3dfb3d25938cd391cf986af
Samira <grossisamira@hotmail.com>
2024-10-02 21:48:45 -0300

    feat: adds Register component and styles

---
+++ src/features/register/Register.styles.ts
@@ -0,0 +1,8 @@
+export const boxStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    gap: 4,
+}
---
+++ src/features/register/Register.tsx
@@ -0,0 +1,79 @@
+import { FC, useState } from 'react'
+import { Box, Button, Grid } from '@chakra-ui/react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { z } from 'zod'
+
+import { Alert, CustomBox, FormInput } from 'components'
+import { validationSchema, initialValues } from './Register.schema'
+import { boxStyles } from './Register.styles'
+import { auth } from 'lib'
+
+export type RegisterProps = z.infer<typeof validationSchema>
+
+export const Register: FC = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<RegisterProps>({
+        resolver: zodResolver(validationSchema),
+        defaultValues: initialValues,
+    })
+
+    const onSubmit: SubmitHandler<RegisterProps> = (data: RegisterProps) => {
+        setIsLoading(true)
+        try {
+            const crendentials = createUserWithEmailAndPassword(auth, data.email, data.password)
+            console.log(crendentials)
+            Alert({ message: 'Seus dados foram cadastrados!' })
+        } catch (error: any) {
+            if (error.message === 'auth/email-already-exists') {
+                Alert({ message: 'O usuário já existe.', type: 'error' })
+            }
+
+            // if (error.message === 'auth/phone-number-already-exists') {
+            //     Alert({ message: 'O número de telefone já foi cadastrado.', type: 'error' })
+            // }
+
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <Grid templateColumns="repeat(2, 1fr)">
+            <Box bg="blue_black" height="100vh" />
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <CustomBox sx={boxStyles}>
+                    <FormInput placeholder="Digite seu nome" errors={errors.name} {...register('name')} />
+                    <FormInput placeholder="Digite seu celular" errors={errors.cellPhone} {...register('cellPhone')} />
+                    <FormInput
+                        type="email"
+                        placeholder="Digite seu email"
+                        errors={errors.email}
+                        {...register('email')}
+                    />
+                    <FormInput
+                        type="password"
+                        placeholder="Digite sua senha"
+                        errors={errors.password}
+                        {...register('password')}
+                    />
+                    <Box display="flex" justifyContent="space-between" gap={4} mt={4}>
+                        <Button width="100%" variant="outline" color="blue_black">
+                            Voltar
+                        </Button>
+                        <Button width="100%" type="submit" bg="blue_black" color="light_gray">
+                            Enviar
+                        </Button>
+                    </Box>
+                </CustomBox>
+            </form>
+        </Grid>
+    )
+}
